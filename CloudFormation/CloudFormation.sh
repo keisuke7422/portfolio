c775a4a0789fbd129bef7afe9c80a8f45474ab3b
keisuke7422 <growing182@gmail.com>
2023-10-14 15:31:12 +0900

    add CloudFormation.sh

--- CloudFormation/CloudFormation.sh
+++ CloudFormation/CloudFormation.sh
@@ -3,4 +3,13 @@
 CFN_STACK_NAME=VPC-test
 CFN_TEMPLATE=file://vpc.yaml
 
-aws cloudformation create-stack --stack-name ${CFN_STACK_NAME} --template-body ${CFN_TEMPLATE}
+if [ "$1" == "new" ]; then
+    echo "create-stack"
+    aws cloudformation create-stack --stack-name ${CFN_STACK_NAME} --template-body ${CFN_TEMPLATE}
+elif [ "$1" == "del" ]; then
+    echo "delete-stack"
+    aws cloudformation delete-stack --stack-name ${CFN_STACK_NAME}
+else
+    echo "create-change-set"
+    aws cloudformation create-change-set --stack-name ${CFN_STACK_NAME} --change-set-name ${CFN_STACK_NAME} --template-body ${CFN_TEMPLATE}
+fi
